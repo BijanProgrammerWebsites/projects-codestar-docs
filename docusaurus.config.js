1383a8389a770298d0863a74dfcdb25f33bc220d
BijanProgrammer <bijaneisapour@gmail.com>
2024-01-31 09:27:21 +0330

    [UPDATE] project name and base url

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,7 +10,7 @@ const config = {
     staticDirectories: ['public', 'static'],
     tagline: 'بزن بریم!',
     url: 'https://star-academy.github.io/',
-    baseUrl: '/codestar-documents/',
+    baseUrl: '/',
     onBrokenLinks: 'throw',
     onBrokenMarkdownLinks: 'warn',
     favicon: 'images/favicon.ico',
@@ -18,7 +18,7 @@ const config = {
     // GitHub pages deployment config.
     // If you aren't using GitHub pages, you don't need these.
     organizationName: 'Star-Academy', // Usually your GitHub org/user name.
-    projectName: 'codestar-documents', // Usually your repo name.
+    projectName: 'codestar-docs', // Usually your repo name.
 
     // Even if you don't use internalization, you can use this field to set useful
     // metadata like html lang. For example, if your site is Chinese, you may want
@@ -41,7 +41,7 @@ const config = {
             ({
                 docs: {
                     sidebarPath: require.resolve('./sidebars.js'),
-                    editUrl: 'https://github.com/Star-Academy/codestar-documents/tree/master/',
+                    editUrl: 'https://github.com/BijanProgrammerWebsites/projects-codestar-docs/tree/master/',
                 },
                 blog: {
                     showReadingTime: true,
