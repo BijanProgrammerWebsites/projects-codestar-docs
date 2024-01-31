7e65fede5e49aab69e1cbeb4d025c579c4b1505a
BijanProgrammer <bijaneisapour@gmail.com>
2024-01-31 13:13:17 +0330

    [ADD] sass support

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,6 +20,8 @@ const config = {
     organizationName: 'Star-Academy', // Usually your GitHub org/user name.
     projectName: 'codestar-docs', // Usually your repo name.
 
+    plugins: ['docusaurus-plugin-sass'],
+
     // Even if you don't use internalization, you can use this field to set useful
     // metadata like html lang. For example, if your site is Chinese, you may want
     // to replace "en" with "zh-Hans".
